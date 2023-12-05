ae1880848e8e0b9164baff45e88031a451ba6e67
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-12-05 15:20:46 +0100

    fix: shoes test

--- src/components/Shoes/Shoes.test.jsx
+++ src/components/Shoes/Shoes.test.jsx
@@ -2,8 +2,14 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import Shoes from "./Shoes";
 
 it("should allow user to select shoe size for each player", () => {
-  const mockUpdateSize = () => {};
-  const mockAddShoe = () => {};
+  let mockUpdateSizeCalled = false;
+  const mockUpdateSize = () => {
+    mockUpdateSizeCalled = true;
+  };
+  let mockAddShoeCalled = false;
+  const mockAddShoe = () => {
+    mockAddShoeCalled = true;
+  };
   const mockRemoveShoe = () => {};
   const shoes = [{ id: "shoe1", size: "" }];
 
@@ -18,14 +24,25 @@ it("should allow user to select shoe size for each player", () => {
 
   const addButton = screen.getByText("+");
   fireEvent.click(addButton);
+
+  expect(mockAddShoeCalled).toBe(true);
 });
 
 it("should allow user to remove a shoe size field", () => {
-  const mockRemoveShoe = () => {};
-  const shoes = [{ id: "shoe1", size: "" }];
+  let mockRemoveShoeCalled = false;
+  const mockRemoveShoe = (id) => {
+    if (id === "shoe1") {
+      mockRemoveShoeCalled = true;
+    }
+  };
+  const shoes = [
+    { id: "shoe1", size: "" },
+    { id: "shoe2", size: "" },
+  ];
 
   render(<Shoes removeShoe={mockRemoveShoe} shoes={shoes} />);
 
-  const removeButton = screen.getByText("-");
-  fireEvent.click(removeButton);
+  const removeButtons = screen.getAllByText("-");
+  fireEvent.click(removeButtons[0]);
+  expect(mockRemoveShoeCalled).toBe(true);
 });
