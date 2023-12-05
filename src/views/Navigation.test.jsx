9a19723960d8e82fdfa459eead01fa17bd281c06
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-12-05 22:44:20 +0100

    add: navigation

--- src/views/Navigation.test.jsx
+++ src/views/Navigation.test.jsx
@@ -1,30 +1,46 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 import Booking from "../views/Booking";
 import Confirmation from "../views/Confirmation";
+import { expect, vi } from "vitest";
 
 describe("Navigation between booking and confirmation views", () => {
   it("navigates to the confirmation view after submitting a reservation", async () => {
     render(
-      <MemoryRouter initialEntries={["/booking"]}>
+      <MemoryRouter initialEntries={["//confirmation"]}>
         <Routes>
-          <Route path="/booking" element={<Booking />} />
+          <Route path="/" element={<Booking />} />
           <Route path="/confirmation" element={<Confirmation />} />
         </Routes>
       </MemoryRouter>
     );
 
-    const confirmButton = screen.getByRole("button", { name: /strIIIIIike!/i });
-    fireEvent.click(confirmButton);
+    userEvent.type(screen.getByLabelText(/Date/i), "2023-12-05");
+
+    userEvent.click(screen.getByText(/strIIIIIike!/i));
+
+    await waitFor(() => {
+      expect(window.location.pathname).toBe("/confirmation");
+    });
   });
 
-  it("shows all relevant booking information on the confirmation view", async () => {});
+  it("shows all relevant booking information on the confirmation view", async () => {
+    render(
+      <MemoryRouter initialEntries={["/confirmation"]}>
+        <Routes>
+          <Route path="/" element={<Booking />} />
+          <Route path="/confirmation" element={<Confirmation />} />
+        </Routes>
+      </MemoryRouter>
+    );
+  });
 
   it("provides a way to navigate back to the booking view from the confirmation view", () => {
     render(
       <MemoryRouter initialEntries={["/confirmation"]}>
         <Routes>
-          <Route path="/booking" element={<Booking />} />
+          <Route path="/" element={<Booking />} />
           <Route path="/confirmation" element={<Confirmation />} />
         </Routes>
       </MemoryRouter>
