84361d6c3149fa22c73e8613d574dd3bf231aac0
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-12-06 12:42:56 +0100

    add: navigation test

---
+++ src/Navigation.test.jsx
@@ -0,0 +1,31 @@
+import { beforeEach, it } from "vitest";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import App from "./App";
+
+describe("Testing navigation ", () => {
+  beforeEach(() => {
+    render(<App />);
+    fireEvent.click(screen.getByTestId("navigation-item"));
+  });
+
+  it(" Should have 2 navigation links when clicked on the menu bar icon", () => {
+    const navigationLinks = screen.getAllByTestId("navigation-link");
+    expect(navigationLinks.length).toBe(2);
+    expect(navigationLinks[0].textContent).toBe("Booking");
+    expect(navigationLinks[1].textContent).toBe("Confirmation");
+  });
+
+  // it("Should redirect to the booking page if clicked on the booking navigate link", () => {
+  //   const navigationLinks = screen.getAllByTestId("navigation-link");
+  //   fireEvent.click(navigationLinks[0]);
+  //   expect(screen.queryByText("When, WHAT & Who")).toBeInTheDocument();
+  // });
+
+  // it("Should redirect to the confirmation page if clicked on the confirmation navigate link", async () => {
+  //   const navigationLinks = screen.getAllByTestId("navigation-link");
+  //   fireEvent.click(navigationLinks[1]);
+  //   await waitFor(() => {
+  //     expect(screen.queryByText("Inga bokning gjord!")).toBeInTheDocument();
+  //   });
+  // });
+});
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,22 +1,44 @@
-import './Navigation.scss';
-import navicon from '../../assets/navicon.svg';
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import "./Navigation.scss";
+import navicon from "../../assets/navicon.svg";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Navigation() {
-    const navigate = useNavigate();
-    const [showMenu, setShowMenu] = useState(false);
+  const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
 
-    return (
-        <nav className={ `navigation ${ showMenu ? 'show-menu' : '' }` }>
-            <img src={ navicon } className='navigation__icon'
-            onClick={ () => { setShowMenu(!showMenu) }} />
-            <a href="#" className={ `navigation__link ${ showMenu ? '' : 'hide' }` } 
-            onClick={ () => { navigate('/') }}>Booking</a>
-            <a href="#" className={ `navigation__link ${ showMenu ? '' : 'hide' }` } 
-            onClick={ () => { navigate('/confirmation') }}>Confirmation</a>
-        </nav>
-    )
+  return (
+    <nav className={`navigation ${showMenu ? "show-menu" : ""}`}>
+      <img
+        src={navicon}
+        className="navigation__icon"
+        onClick={() => {
+          setShowMenu(!showMenu);
+        }}
+        data-testid="navigation-item"
+      />
+      <a
+        href="#"
+        className={`navigation__link ${showMenu ? "" : "hide"}`}
+        onClick={() => {
+          navigate("/");
+        }}
+        data-testid="navigation-link"
+      >
+        Booking
+      </a>
+      <a
+        href="#"
+        className={`navigation__link ${showMenu ? "" : "hide"}`}
+        onClick={() => {
+          navigate("/confirmation");
+        }}
+        data-testid="navigation-link"
+      >
+        Confirmation
+      </a>
+    </nav>
+  );
 }
 
 export default Navigation;
