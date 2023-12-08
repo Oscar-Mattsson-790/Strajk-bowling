c18fb8122400e0247d694446dc2fe4727fc3e5a7
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-12-08 07:58:25 +0100

    test ok

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,11 +1,14 @@
-import './ErrorMessage.scss';
+import "./ErrorMessage.scss";
 
 function ErrorMessage() {
-    return (
-        <article className='error-message'>
-            <p className='error-message__text'>Fill out all the fields and make sure that people and shoes is the same number.</p>
-        </article>
-    )
+  return (
+    <article className="error-message">
+      <p className="error-message__text">
+        Fill out all the fields and make sure that people and shoes is the same
+        number.
+      </p>
+    </article>
+  );
 }
 
 export default ErrorMessage;
